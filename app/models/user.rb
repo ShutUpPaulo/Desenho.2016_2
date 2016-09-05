fd9d428baf25c5704bb37cd3a6d295f7088ad576
duerno <felipeduernoal@gmail.com>
2016-09-05 12:26:26 -0300

    Add devise views for Users and remove ORM :recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Others available are:
+  # :confirmable, :lockable, :timeoutable, :recoverable, :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
+
+  # Custom validations
+  # validate(:validate_username)
+
+  # private
+  #   # Fix the conflict between username and email
+  #   def validate_username
+  #     if User.where(email: username).exists?
+  #       errors.add(:username, :invalid)
+  #     end
+  #   end
 end
