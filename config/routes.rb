1f16b9fff5839ab9d3eb25509e417e2d9b641c79
ShutUpPaulo <markes.calado@gmail.com>
2016-10-02 20:39:02 -0300

    Modify the pages controller system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'pages#index'
   devise_for :users, controllers: {
     # confirmations:      'users/confirmations',
     # omniauth_callbacks: 'users/omniauth_callbacks',
