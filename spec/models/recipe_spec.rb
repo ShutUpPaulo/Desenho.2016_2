6e92abda89b34fc0f11420addcbdf309e2e151d8
Fabio Teixeira <fabio1079@gmail.com>
2016-11-09 12:42:28 -0200

    Add unit tests for recipe
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'saves a valid recipe' do
+    #pending: Add proper validations to Recipe model
+    # recipe can save even with blank fields...
+    recipe = Recipe.new
+    expect(recipe.validate).to eq(true)
+    expect(recipe.save).to eq(true)
+  end
+
+  it 'acts as taggable' do
+    recipe = Recipe.create!
+
+    recipe.tag_list.add 'first'
+    recipe.tag_list.add 'second'
+
+    recipe.save!
+    expect(recipe.tag_list).to eq(['first', 'second'])
+  end
 end
